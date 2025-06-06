8544917fd2ea0ab5167dc368f18019e044194e24
Yael Pilar <yaelpilarluque@gmail.com>
2025-06-06 13:48:58 +0000

    refactor(itemGroups.tsx): new file with item groups

---
+++ app/itemGroups.tsx
@@ -0,0 +1,91 @@
+import type * as v from "valibot";
+import type { ItemSchema } from "~/schema";
+
+type Item = v.InferOutput<typeof ItemSchema>;
+
+export function getBootItems(lolItems: Item[]) {
+  return lolItems.filter((item) => item.tags.includes("Boots"));
+}
+
+export function getConsumableItems(lolItems: Item[]) {
+  return lolItems.filter(
+    (item) =>
+      item.tags.includes("Consumable") ||
+      !item.stats ||
+      (item.gold.base === 0 && item.gold.sell === 0),
+  );
+}
+
+export function getStarterItems(lolItems: Item[]) {
+  const tagsInclude = ["Lane", "Jungle"];
+  const tagsExclude = ["Consumable"];
+
+  const starterItems = lolItems
+    .filter((item) => {
+      return item.tags.some((tag) => tagsInclude.includes(tag));
+    })
+    .filter((item) => {
+      return !item.tags.some((tag) => tagsExclude.includes(tag));
+    })
+    .filter((item) => {
+      return !item.from && item.gold.base > 0;
+    })
+    .filter((item) => {
+      return !(
+        item.tags.length === 2 &&
+        item.tags.includes("Damage") &&
+        item.tags.includes("Lane")
+      );
+    });
+
+  const uniqueStarterItems = Array.from(
+    new Map(starterItems.map((item) => [item.name, item])).values(),
+  );
+
+  return uniqueStarterItems;
+}
+
+export function getBasicItems(lolItems: Item[]) {
+  return lolItems
+    .filter((item) => {
+      return !item.from;
+    })
+    .filter((item) => {
+      return !item.tags.includes("Boots");
+    })
+    .filter((item) => {
+      return item.into && item.into.length >= 3;
+    })
+    .filter((item) => {
+      return !(item.tags.includes("Mana") && item.tags.includes("ManaRegen"));
+    });
+}
+
+export function getEpicItems(lolItems: Item[]) {
+  return lolItems
+    .filter((item) => {
+      return item.depth === 2 || item.consumeOnFull === true;
+    })
+    .filter((item) => {
+      return item.consumeOnFull === true || (item.into && item.from);
+    })
+    .filter((item) => {
+      return !item.tags.includes("Boots");
+    })
+    .filter((item) => {
+      return !(item.stats && item.stats.FlatHPPoolMod === 250);
+    });
+}
+
+export function getLengedaryItems(lolItems: Item[]) {
+  return lolItems
+    .filter((item) => {
+      return !item.into && item.from;
+    })
+    .filter((item) => {
+      return item.depth === 2 || item.depth === 3;
+    })
+    .filter((item) => {
+      return !item.tags.includes("Boots");
+    });
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,14 @@ import { useEffect, useReducer } from "react";
 import "~/styles/app.css";
 import * as v from "valibot";
 import { ItemSchema, ItemsResponseSchema, DataSchema } from "~/schema";
+import {
+  getBootItems,
+  getBasicItems,
+  getConsumableItems,
+  getEpicItems,
+  getLengedaryItems,
+  getStarterItems,
+} from "~/itemGroups";
 
 type Item = v.InferOutput<typeof ItemSchema>;
 
@@ -276,11 +284,7 @@ function storeItemCard(
 
       <button
         type="button"
-        className={
-          isPurchased || !isPurchased
-            ? "available-button"
-            : "unavailable-button"
-        }
+        className={isPurchased ? "available-button" : "unavailable-button"}
         onClick={() => handleClickSell(selectedItem)}
       >
         Sell
@@ -355,91 +359,4 @@ function itemCard(item: Item, handleClick: (item: Item) => void) {
       <span>{item.gold.total}</span>
     </li>
   );
-}
-
-function getBootItems(lolItems: Item[]) {
-  return lolItems.filter((item) => item.tags.includes("Boots"));
-}
-
-function getConsumableItems(lolItems: Item[]) {
-  return lolItems.filter(
-    (item) =>
-      item.tags.includes("Consumable") ||
-      !item.stats ||
-      (item.gold.base === 0 && item.gold.sell === 0),
-  );
-}
-
-function getStarterItems(lolItems: Item[]) {
-  const tagsInclude = ["Lane", "Jungle"];
-  const tagsExclude = ["Consumable"];
-
-  const starterItems = lolItems
-    .filter((item) => {
-      return item.tags.some((tag) => tagsInclude.includes(tag));
-    })
-    .filter((item) => {
-      return !item.tags.some((tag) => tagsExclude.includes(tag));
-    })
-    .filter((item) => {
-      return !item.from && item.gold.base > 0;
-    })
-    .filter((item) => {
-      return !(
-        item.tags.length === 2 &&
-        item.tags.includes("Damage") &&
-        item.tags.includes("Lane")
-      );
-    });
-
-  const uniqueStarterItems = Array.from(
-    new Map(starterItems.map((item) => [item.name, item])).values(),
-  );
-
-  return uniqueStarterItems;
-}
-
-function getBasicItems(lolItems: Item[]) {
-  return lolItems
-    .filter((item) => {
-      return !item.from;
-    })
-    .filter((item) => {
-      return !item.tags.includes("Boots");
-    })
-    .filter((item) => {
-      return item.into && item.into.length >= 3;
-    })
-    .filter((item) => {
-      return !(item.tags.includes("Mana") && item.tags.includes("ManaRegen"));
-    });
-}
-
-function getEpicItems(lolItems: Item[]) {
-  return lolItems
-    .filter((item) => {
-      return item.depth === 2 || item.consumeOnFull === true;
-    })
-    .filter((item) => {
-      return item.consumeOnFull === true || (item.into && item.from);
-    })
-    .filter((item) => {
-      return !item.tags.includes("Boots");
-    })
-    .filter((item) => {
-      return !(item.stats && item.stats.FlatHPPoolMod === 250);
-    });
-}
-
-function getLengedaryItems(lolItems: Item[]) {
-  return lolItems
-    .filter((item) => {
-      return !item.into && item.from;
-    })
-    .filter((item) => {
-      return item.depth === 2 || item.depth === 3;
-    })
-    .filter((item) => {
-      return !item.tags.includes("Boots");
-    });
 }
